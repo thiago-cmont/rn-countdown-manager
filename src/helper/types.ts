6abfbdb2cd47ea810fdd57cdb816b269f1e4b43a
Thiago Correia <thiagocorreiamonteiro@gmail.com>
2024-07-30 18:01:06 -0300

    feat: fix tests

--- src/helper/types.ts
+++ src/helper/types.ts
@@ -1,4 +1,4 @@
-import { CreateArrayWithLengthX, NumericRange } from '../../timer/hooks/types';
+import { CreateArrayWithLengthX, NumericRange } from '../timer/hooks/types';
 
 export type ZERO_TO_NINE = NumericRange<CreateArrayWithLengthX<0>, 9>;
 export type getBlocksTestIdAndAmountFromNumberReturn = Map<string, number>;
